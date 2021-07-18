e57d9f11dd2d18227af7198dc5a90f57d2fe5733
Nathanacus <4307854+Nathanacus@users.noreply.github.com>
2021-07-18 00:48:38 -0500

    Complete and text MixedMessages.js

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,7 +1,75 @@
-<script>
+// //////////////////////////
+// Arrays ///////////////////
+// //////////////////////////
 
-    // Store topic words in arrays
-    const arrayCats = ['caracal', 'cheetah', 'cougar', 'jaguar', 'leopard', 'lion', 'lynx', 'ocelot', 'serval', 'tiger'];
-    const arrayTrees = ['apple', 'banyan', 'baobab', 'fir', 'magnolia', 'oak', 'pine', 'spruce', 'sycamore', 'willow'];
+// List of cat species
+const arrayCats = [
+  "caracal",
+  "cheetah",
+  "cougar",
+  "jaguar",
+  "leopard",
+  "lion",
+  "lynx",
+  "ocelot",
+  "serval",
+  "tiger",
+];
 
-</script>
+// List of tree species
+const arrayTrees = [
+  "apple",
+  "banyan",
+  "baobab",
+  "fir",
+  "magnolia",
+  "oak",
+  "pine",
+  "spruce",
+  "sycamore",
+  "willow",
+];
+
+// //////////////////////////
+// Functions ////////////////
+// //////////////////////////
+
+// Function to determine whether first letter of a word is a vowel
+function startsWithVowel(str) {
+  switch (str[0]) {
+    case "a":
+      return true;
+    case "e":
+      return true;
+    case "i":
+      return true;
+    case "o":
+      return true;
+    case "u":
+      return true;
+    default:
+      return false;
+  }
+}
+
+// //////////////////////////
+// Main Script //////////////
+// //////////////////////////
+
+// Randomly select a cat and a tree
+let cat = arrayCats[Math.floor(Math.random() * 9)];
+let tree = arrayTrees[Math.floor(Math.random() * 9)];
+
+// Check if "A" or "An" is appropriate depending on species of cat
+if (startsWithVowel(cat)) {
+  cat = "n " + cat;
+} else {
+  cat = " " + cat;
+}
+
+// Get the current date
+let today = new Date();
+let date = today.getMonth() + "/" + today.getDate() + "/" + today.getFullYear();
+
+// Output message to the user
+console.log(`A${cat} climbed up the ${tree} tree on ${date}!`);
