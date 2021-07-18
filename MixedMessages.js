b9fb827bb714bd630f5ef99df9c56c8bd84b5d92
Nathanacus <4307854+Nathanacus@users.noreply.github.com>
2021-07-18 00:23:36 -0500

    Create MixedMessages.js and add topic word arrays

---
+++ MixedMessages.js
@@ -0,0 +1,7 @@
+<script>
+
+    // Store topic words in arrays
+    const arrayCats = ['caracal', 'cheetah', 'cougar', 'jaguar', 'leopard', 'lion', 'lynx', 'ocelot', 'serval', 'tiger'];
+    const arrayTrees = ['apple', 'banyan', 'baobab', 'fir', 'magnolia', 'oak', 'pine', 'spruce', 'sycamore', 'willow'];
+
+</script>
